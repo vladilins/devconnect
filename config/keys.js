30af5c04a787658eb25439f7fad4a1aa6c59d25f
Vlad Ilin <ilin96inbox@gmail.com>
2018-11-27 15:17:34 +0800

    devconnect

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://vlad:Vlad1996@ds117834.mlab.com:17834/mernproject"
+};
---
+++ routes/api/posts.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// @route Get api/posts/test
+// @desc Tests posts route
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+module.exports = router;
