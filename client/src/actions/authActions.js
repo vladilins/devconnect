e4a9f4242ac8ae7fed054613151e43a0ad1ff1b7
Vlad Ilin <ilin96inbox@gmail.com>
2018-11-29 12:52:52 +0800

    Redux Login Action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utlis/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+import jwt_decode from "jwt-decode";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +15,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login Get user
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/user/login", userData)
+    .then(res => {
+      // Save to local storage
+      const { token } = res.data;
+      // Set token to local
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
